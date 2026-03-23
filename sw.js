90b88890fd422050a44d6bb5f9c6da04ca102a37
reelsrundown-spec <reelsrundown@gmail.com>
2026-03-23 13:39:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('fetch', function(event) {
-  // Service worker is active
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
